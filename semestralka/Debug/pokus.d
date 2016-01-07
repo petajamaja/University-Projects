283134d6b7034acfe29d88a13af68b5e1654fedc
Natalia Bogutskaya <Mac@Mac-MacBook-Pro.local>
2016-01-07 05:37:57 +0100

    deleted useless coursework

--- semestralka/Debug/pokus.d
+++
@@ -1,6 +0,0 @@
-pokus.d pokus.o: ../pokus.c ../mmaped_8bit_bus_kbd-headers-2013/kbd_hw.h \
- ../mmaped_8bit_bus_kbd-headers-2013/chmod_lcd.h
-
-../mmaped_8bit_bus_kbd-headers-2013/kbd_hw.h:
-
-../mmaped_8bit_bus_kbd-headers-2013/chmod_lcd.h:
